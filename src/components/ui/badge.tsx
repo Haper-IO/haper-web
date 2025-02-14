efe9db6c45b47f6be616cf2d69db8a3e0e7963c9
Kaige Zheng <zheng.kaig@northeastern.edu>
2025-02-14 04:24:38 -0500

    updated badge styles

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -9,9 +9,9 @@ const badgeVariants = cva(
     variants: {
       variant: {
         default:
-          "border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80",
+          "border-transparent bg-slate-600 text-slate-50 shadow",
         secondary:
-          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "border-transparent bg-slate-200 text-slate-500",
         destructive:
           "border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80",
         outline: "text-foreground",
